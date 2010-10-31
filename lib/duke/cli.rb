bff55dcfb1d90a72a25ac4a78112efd367bd44d8
Justin Marney <gotascii@gmail.com>
2010-10-31 10:31:11 -0400

    testin out the funk

--- lib/duke/cli.rb
+++ lib/duke/cli.rb
@@ -1,6 +1,6 @@
 module Duke
   class Cli < Thor
-    desc "start REPO_NAME PORT", "start cijoe for REPO_NAME on port PORT"
+    desc "start REPO_NAME PORT", "start cijoe for REPO_NAME on PORT"
     def start(repo_name, port)
       Controller.new(repo_name, port).start
     end
