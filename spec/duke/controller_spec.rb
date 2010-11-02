482ae71def8016374f301437df7dc195687b4ed0
Justin Marney <gotascii@gmail.com>
2010-11-02 11:08:11 -0400

    coverin up controller

--- spec/duke/controller_spec.rb
+++ spec/duke/controller_spec.rb
@@ -1,59 +1,78 @@
-describe "An instance of Controller" do
+require 'spec_helper'
+
+describe Controller do
   before do
-    @repo_name = 'repo'
-    @port = 4567
-    @controller = Controller.new(@repo_name, @port)
+    ::Duke::Config.stub(:log_dir).and_return("log_dir")
+    ::Duke::Config.stub(:pid_dir).and_return("pid_dir")
+    @controller = Controller.new('repo_dir', 4567)
     @dir = Dir.pwd
   end
 
-  it "has a dir that matches cwd" do
-    @controller.dir.should == @dir
+  describe "#initialize(repo_dir, port)" do
+    it "has a dir that matches cwd" do
+      @controller.dir.should == @dir
+    end
+
+    it "has a repo_dir" do
+      @controller.repo_dir.should == 'repo_dir'
+    end
+
+    it "has a port reader" do
+      @controller.port.should == 4567
+    end
+
+    it "has a port writer" do
+      @controller.port = 1234
+      @controller.port.should == 1234
+    end
+
+    it "has a timeout of 7 seconds" do
+      @controller.timeout.should == 7
+    end
   end
 
-  it "has a repo_name" do
-    @controller.repo_name.should == @repo_name
+  describe "#identifier" do
+    it "identifies the controller via repo_dir and port" do
+      @controller.identifier.should == "repo_dir.4567"
+    end
   end
 
-  it "has a port" do
-    @controller.port.should == @port
+  describe "#pid_file" do
+    it "is the path to the pid file" do
+      @controller.pid_file.should == "#{@dir}/pid_dir/repo_dir.4567.pid"
+    end
   end
 
-  it "has an identifier made up of repo_name and port" do
-    @controller.identifier.should == "repo.4567"
+  describe "#log_file" do
+    it "is the path to the log file" do
+      @controller.log_file.should == "#{@dir}/log_dir/repo_dir.4567.log"
+    end
   end
 
-  it "has a pid_file" do
-    @controller.pid_file.should == "#{@dir}/tmp/pids/repo.4567.pid"
+  describe "#ping_command" do
+    it "verifies that cijoe is running" do
+      TCPSocket.should_receive(:new).with('localhost', 4567).and_return('sockette!')
+      @controller.ping_command.call.should == 'sockette!'
+    end
   end
 
-  it "has a log_file" do
-    @controller.log_file.should == "#{@dir}/log/repo.4567.log"
-  end
-
-  it "has a ping command" do
-    TCPSocket.should_receive(:new).with('localhost', 4567).and_return('sockette!')
-    @controller.ping_command.call.should == 'sockette!'
-  end
-
-  it "has a timeout of 7 seconds" do
-    @controller.timeout.should == 7
-  end
-
-  it "has a DaemonController instance" do
-    @controller.stub(:identifier).and_return("identifier!")
-    @controller.stub(:ping_command).and_return("ping_command!")
-    @controller.stub(:pid_file).and_return("pid_file!")
-    @controller.stub(:log_file).and_return("log_file!")
-    args = {
-      :identifier    => "identifier!",
-      :start_command => "duke cijoed repo 4567 log_file! pid_file!",
-      :ping_command  => "ping_command!",
-      :pid_file      => "pid_file!",
-      :log_file      => "log_file!",
-      :log_file_activity_timeout => 7,
-      :start_timeout => 7
-    }
-    DaemonController.should_receive(:new).with(args).and_return("controller!")
-    @controller.controller.should == "controller!"
+  describe "#controller" do
+    it "instantiates a DaemonController instance" do
+      @controller.stub(:identifier).and_return("identifier")
+      @controller.stub(:ping_command).and_return("ping_command")
+      @controller.stub(:pid_file).and_return("pid_file")
+      @controller.stub(:log_file).and_return("log_file")
+      args = {
+        :identifier    => "identifier",
+        :start_command => "duke cijoed repo_dir 4567 log_file pid_file",
+        :ping_command  => "ping_command",
+        :pid_file      => "pid_file",
+        :log_file      => "log_file",
+        :log_file_activity_timeout => 7,
+        :start_timeout => 7
+      }
+      DaemonController.should_receive(:new).with(args).and_return("controller")
+      @controller.controller.should == "controller"
+    end
   end
 end
